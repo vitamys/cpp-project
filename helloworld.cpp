b0cf3d3263d28f295b24151e840a826bde21680b
Mena Lerf <lerfme@pop-os.localdomain>
2023-09-26 16:52:21 +0200

    exercise first week, in class week2@

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <stack>
+
+
+static bool odd(int n) { return n % 2; }
+
+int main()
+{
+
+		std::cout << "Hello World" << std::endl;
+	return 0;
+}
