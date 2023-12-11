8e14ac53ae89b720a28190363cd3981fb2cf73f6
Mena Lerf <mena.lerf@students.unibe.ch>
2023-12-11 17:15:48 +0100

    clean up

--- Lesson07/Demo07_async/Demo07_upload/main4.cpp
+++
@@ -1,5 +0,0 @@
-// Launch policies
-future<int> fu = async(factorial, 4);
-future<int> fu = async(launch::deferred, factorial, 4);
-future<int> fu = async(launch::async, factorial, 4);
-future<int> fu = async(launch::async | launch::deferred,factorial,4);
